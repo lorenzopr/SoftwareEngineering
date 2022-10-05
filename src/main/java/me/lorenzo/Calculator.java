4cabf261c8c5db2042750ed6ab788427114f8fbd
Lorenzo <lorenzoparisi1999@gmail.com>
2022-10-05 17:50:33 +0200

    feat: add calculator

---
+++ src/main/java/me/lorenzo/Calculator.java
@@ -0,0 +1,16 @@
+package me.lorenzo;
+
+public class Calculator {
+
+	static int sum(int a, int b){
+		return a + b;
+	}
+
+	static int sub(int a, int b){
+		return a - b;
+	}
+
+	static int multiply(int a, int b){
+		return a * b;
+	}
+}
